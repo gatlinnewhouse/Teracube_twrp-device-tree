af058c6e1ed853fc5af6ad132c8a2f94c5f91449
Gatlin Newhouse <gatlinnewhouse@users.noreply.github.com>
2020-07-08 12:38:04 -0700

    Adding crypto flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,3 +68,9 @@ TW_H_OFFSET := -80
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
