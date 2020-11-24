a1e6c102662bc58b9a15affa2054b4a4119d597a
Gatlin Newhouse <gatlinnewhouse@users.noreply.github.com>
2020-11-23 16:25:59 -0800

    kernel source build

--- omni_Teracube_One.mk
+++ omni_Teracube_One.mk
@@ -16,21 +16,54 @@
 # limitations under the License.
 #
 
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Specify phone tech before including full_phone
-#$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit language packages
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit 64bit support
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+#PRODUCT_PACKAGES += \
+#    android.hardware.boot@1.0-impl \
+#    android.hardware.boot@1.0-service
+
+#PRODUCT_PACKAGES += \
+#    bootctrl.$(TARGET_BOARD_PLATFORM)
+    
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#    bootctrl.$(TARGET_BOARD_PLATFORM) \
+#    libgptutils \
+#    libz \
+#    libcutils
+    
+#PRODUCT_PACKAGES += \
+#    otapreopt_script \
+#    cppreopts.sh \
+#    update_engine \
+#    update_verifier \
+#    update_engine_sideload
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Teracube_One
@@ -54,36 +87,3 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     ro.debuggable=1 \
     ro.allow.mock.location=0
-
-# A/B
-#AB_OTA_PARTITIONS += \
-#    boot \
-#    system \
-#    vendor
-
-#AB_OTA_POSTINSTALL_CONFIG += \
-#    RUN_POSTINSTALL_system=true \
-#    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-#    FILESYSTEM_TYPE_system=ext4 \
-#    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-#PRODUCT_PACKAGES += \
-#    android.hardware.boot@1.0-impl \
-#    android.hardware.boot@1.0-service
-
-#PRODUCT_PACKAGES += \
-#    bootctrl.$(TARGET_BOARD_PLATFORM)
-    
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.$(TARGET_BOARD_PLATFORM) \
-#    libgptutils \
-#    libz \
-#    libcutils
-    
-#PRODUCT_PACKAGES += \
-#    otapreopt_script \
-#    cppreopts.sh \
-#    update_engine \
-#    update_verifier \
-#    update_engine_sideload
