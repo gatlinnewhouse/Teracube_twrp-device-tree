21e1d3efebeca54ceffb5166fb70ba82b415bf3d
Gatlin Newhouse <gatlinnewhouse@users.noreply.github.com>
2020-11-24 10:38:59 -0800

    Using crypto options from fstab
    And from mrmazakblu's branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,13 @@ TW_INCLUDE_NTFS_3G := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_INCLUDE_CRYPTO := true
 #TW_CRYPTO_USE_SYSTEM_VOLD := true
 TWRP_INCLUDE_LOGCAT := true
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+#TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
